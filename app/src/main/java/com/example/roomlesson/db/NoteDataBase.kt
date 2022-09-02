37c7e1f1e5d5c2e1b8f3591a79c2c6811257d3fd
Avdors <avdors@bk.ru>
2022-09-02 21:33:02 +0700

    NoteDataBase

--- app/src/main/java/com/example/roomlesson/db/NoteDataBase.kt
+++ app/src/main/java/com/example/roomlesson/db/NoteDataBase.kt
@@ -1,4 +1,26 @@
 package com.example.roomlesson.db
 
-class NoteDataBase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.roomlesson.db.dao.NotDao
+import com.example.roomlesson.model.NoteModel
+
+@Database(entities = [NoteModel::class], version = 1)
+abstract class NoteDataBase: RoomDatabase() {
+    abstract fun getNoteDao(): NotDao
+
+    companion object{
+        private var database: NoteDataBase? = null
+
+        @Synchronized
+        fun getInstance(context: Context): NoteDataBase{
+            return if(database == null){
+                database = Room.databaseBuilder(context, NoteDataBase::class.java, "db").build()
+                database as NoteDataBase
+            } else{
+                database as NoteDataBase}
+        }
+    }
 }
--- app/src/main/java/com/example/roomlesson/db/repositiry/NotRealisation.kt
+++ app/src/main/java/com/example/roomlesson/db/repositiry/NotRealisation.kt
@@ -1,4 +1,21 @@
 package com.example.roomlesson.db.repositiry
 
-class NotRealisation {
+import androidx.lifecycle.LiveData
+import com.example.roomlesson.db.dao.NotDao
+import com.example.roomlesson.model.NoteModel
+
+class NotRealisation(private val notDao: NotDao): NotRepository {
+
+    override val allNotes: LiveData<List<NoteModel>>
+        get() = notDao.getAllNote()
+
+    override suspend fun insertNote(noteModel: NoteModel, onSuccess: () -> Unit) {
+        notDao.insert(noteModel)
+        onSuccess()
+    }
+
+    override suspend fun deleteNote(noteModel: NoteModel, onSuccess: () -> Unit) {
+        notDao.delete(noteModel)
+        onSuccess()
+    }
 }
