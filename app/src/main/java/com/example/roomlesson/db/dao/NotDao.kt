010d95de6df3cb7bb32c1a43d3925e67526e97e1
Avdors <avdors@bk.ru>
2022-09-02 21:09:49 +0700

    Note Dao

--- app/src/main/java/com/example/roomlesson/db/dao/NotDao.kt
+++ app/src/main/java/com/example/roomlesson/db/dao/NotDao.kt
@@ -1,4 +1,16 @@
 package com.example.roomlesson.db.dao
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.roomlesson.model.NoteModel
+
+@Dao
 interface NotDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(noteModel: NoteModel)
+    @Delete
+    suspend fun delete(noteModel: NoteModel)
+
+    @Query("SELECT * from note_table")
+    fun getAllNote(): LiveData<List<NoteModel>>
 }
